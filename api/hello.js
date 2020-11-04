f2c670747a09810bc3098637183a2b3cc86c12d4
Abhishek Angira <abhishek@angira.net>
2020-11-04 15:45:10 +0530

    vercel function change

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,8 @@
 export default (req, res) => {
-    const { name = 'World' } = req.query
-    res.status(200).send(`Hello ${name}!`)
-}
+    const { name = "World" } = req.query;
+    res.status(200).send(
+        `Hello ${name}!. You are using node ${process.version}`
+    );
+};
 
 //Function for zeit.angira.net
